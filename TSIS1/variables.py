44ee328d45efb9c0464bf7cf2742e71383de2725
togzhan <t.berikbolova@kbtu.kz>
2024-01-19 09:29:01 +0600

    add index

---
+++ TSIS1/variables.py
@@ -0,0 +1,35 @@
+#1
+carname = "Volvo"
+print(carname)
+
+#2
+x = 50
+print(x)
+
+#3
+x = 5
+y = 10
+print(x + y)
+
+#4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#5
+x , y , z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#6
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#7
+def myfunc():
+    global x
+    x = "fantastic"
